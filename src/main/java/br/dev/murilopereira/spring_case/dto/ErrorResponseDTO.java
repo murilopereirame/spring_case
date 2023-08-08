0a9b22cff7163f4a5eb237e9f18736079ac9733d
Murilo Pereira <mrex@tuta.io>
2023-08-08 15:14:47 -0300

    feat: Added status

--- src/main/java/br/dev/murilopereira/spring_case/dto/ErrorResponseDTO.java
+++ src/main/java/br/dev/murilopereira/spring_case/dto/ErrorResponseDTO.java
@@ -2,4 +2,9 @@ package br.dev.murilopereira.spring_case.dto;
 
 import java.util.ArrayList;
 
-public record ErrorResponseDTO(String message, String code, ArrayList<String> details) {};
+public record ErrorResponseDTO(String message, String code, ArrayList<String> details, String status) {
+    public ErrorResponseDTO(String message, String code, ArrayList<String> details) {
+        this(message, code, details, "ERROR");
+    }
+
+};
