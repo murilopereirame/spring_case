faee9660ae0a4c6fbba536eef5278591a1886af6
Murilo Pereira <mrex@tuta.io>
2023-08-08 15:17:19 -0300

    feat: Added update task by id and find by user

--- src/main/java/br/dev/murilopereira/spring_case/repository/TaskRepository.java
+++ src/main/java/br/dev/murilopereira/spring_case/repository/TaskRepository.java
@@ -2,10 +2,20 @@ package br.dev.murilopereira.spring_case.repository;
 
 import br.dev.murilopereira.spring_case.model.Task;
 import br.dev.murilopereira.spring_case.model.User;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TaskRepository extends CrudRepository<Task, String> {
-    @Query(value = "SELECT * FROM tasks WHERE users_iduser= ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM tasks WHERE users_iduser= ?1 ORDER BY title", nativeQuery = true)
     Iterable<Task> findAllByUserId(String users_iduser);
+
+    @Query(value = "SELECT * FROM tasks WHERE users_iduser= ?1 AND idtask=?2", nativeQuery = true)
+    Task findByUserAndId(String users_iduser, String idtask);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE tasks SET done=?2 WHERE idtask=?1", nativeQuery = true)
+    Integer updateTaskById(String taskId, boolean done);
 }
